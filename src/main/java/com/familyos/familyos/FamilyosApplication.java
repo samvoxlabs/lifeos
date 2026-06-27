36f7027ecc3d8e8b617ef9a6443409bee2d8e5d2
shriram-dev7 <shriramvc@gmail.com>
2026-06-27 16:42:46 -0500

    draft

---
+++ src/main/java/com/familyos/familyos/FamilyosApplication.java
@@ -0,0 +1,13 @@
+package com.familyos.familyos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/familyos/familyos/FamilyosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyos.familyos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
